c965ad5f042bcdfcecda0f2208fc07d62d345384
Israel Gebremariam <israelgebremariam@Israels-MacBook-Pro.local>
2016-10-16 23:35:00 -0400

    edit

--- prob1.py
+++ prob1.py
@@ -1,5 +1,8 @@
 import json
 import requests
+import iso8601
+import datetime
+from datetime import timedelta
 
 githubURL = "https://github.com/israelteru/Code2040-Fellows-Application-Tech-Assessment"
 payload = {'token': 'ef24eaa8e037499e9eb6b829328a1365', 'github': githubURL}
@@ -14,4 +17,44 @@ originalString = r.text
 reversedString = originalString[::-1]
 
 payload = {'token': 'ef24eaa8e037499e9eb6b829328a1365', 'string': reversedString}
+r = requests.post('http://challenge.code2040.org/api/reverse/validate', payload)
+
+payload = {'token': 'ef24eaa8e037499e9eb6b829328a1365'}
+r = requests.post('http://challenge.code2040.org/api/haystack', payload)
+
+needle = json.loads(r.text)['needle']
+haystack = json.loads(r.text)['haystack']
+
+# find the index of a given string in an array
+index = -1
+if needle in haystack:
+    index = haystack.index(needle)
+
+payload = {'token': 'ef24eaa8e037499e9eb6b829328a1365', 'needle': index}
+r = requests.post('http://challenge.code2040.org/api/reverse/validate', payload)
+
+payload = {'token': 'ef24eaa8e037499e9eb6b829328a1365'}
+r = requests.post('http://challenge.code2040.org/api/prefix', payload)
+
+prefix = json.loads(r.text)["prefix"]
+array = json.loads(r.text)["array"]
+
+# exclude all strings with a given prefix in a new array
+answer = [stringElt for stringElt in array if not stringElt.startswith(prefix)]
+
+payload = {'token': 'ef24eaa8e037499e9eb6b829328a1365', 'array': answer}
+r = requests.post('http://challenge.code2040.org/api/reverse/validate', payload)
+
+payload = {'token': 'ef24eaa8e037499e9eb6b829328a1365'}
+r = requests.post('http://challenge.code2040.org/api/dating', payload)
+
+datestamp = json.loads(r.text)["datestamp"]
+interval = json.loads(r.text)["interval"]
+
+# add given number of seconds to a given iso8601 date to find the new date
+datestampToUse = iso8601.parse_date(datestamp)
+intervalToUse = datetime.timedelta(seconds=interval)
+newDatestamp = (datestampToUse + intervalToUse).isoformat
+
+payload = {'token': 'ef24eaa8e037499e9eb6b829328a1365', 'datestamp': newDatestamp}
 r = requests.post('http://challenge.code2040.org/api/reverse/validate', payload)
