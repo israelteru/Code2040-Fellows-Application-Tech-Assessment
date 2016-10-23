a270a4c2bb07a07c4557a683e8fb3c3735a872f7
Israel Gebremariam <israelgebremariam@Israels-MacBook-Pro.local>
2016-10-22 17:17:27 -0700

    final

--- prob3.py
+++ prob3.py
@@ -15,4 +15,5 @@ array = json.loads(r.text)["array"]
 answer = [stringElt for stringElt in array if not stringElt.startswith(prefix)]
 
 payload = {'token': 'ef24eaa8e037499e9eb6b829328a1365', 'array': answer}
+
 r = requests.post('http://challenge.code2040.org/api/prefix/validate', payload)
